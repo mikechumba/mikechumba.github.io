19391b83ac49e4e638d8eaf3650a079231c131c2
Michael Kipchumba <michaelchumba09@gmail.com>
2019-06-04 04:02:30 +0300

    Updates

---
+++ precache-manifest.a7d3e5e27a58ad38ee4ff5e1f904e98d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebc8630bae91328614ed5f4140928f29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc1c128e0358e8555d48",
+    "url": "/static/css/main.bc508cb1.chunk.css"
+  },
+  {
+    "revision": "7baa707b962895072f12",
+    "url": "/static/js/2.db94ef0d.chunk.js"
+  },
+  {
+    "revision": "dc1c128e0358e8555d48",
+    "url": "/static/js/main.bc12b3a3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
